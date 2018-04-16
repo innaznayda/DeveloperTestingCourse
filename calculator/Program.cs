c0d6f0987d311af5c0c0ab63b2d48fb9a57aba1d
innaznayda <innaznayda@gmail.com>
2018-04-16 21:34:18 +0300

    ekf

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -15,32 +15,32 @@ namespace calculator {
            return now.Hours.ToString();
        }
 
-       //private static void Calculate() {
-       //    Console.WriteLine("Enter digits separated by whitespace to get a sum");
-       //    string digits = Console.ReadLine();
-       //    var refult = GetSum(digits);
-       //    if (refult == null) {
-       //        Console.WriteLine("incorrect input");
-       //    } else {
-       //        Console.WriteLine("sum {0}", refult);
-       //    }
-       //    Console.ReadLine();
-       //}
+       private static void Calculate() {
+          Console.WriteLine("Enter digits separated by whitespace to get a sum");
+          string digits = Console.ReadLine();
+          var refult = GetSum(digits);
+          if (refult == null) {
+              Console.WriteLine("incorrect input");
+          } else {
+              Console.WriteLine("sum {0}", refult);
+          }
+          Console.ReadLine();
+       }
 
-       //public static int? GetSum(string digits) {
-       //     if (string.IsNullOrEmpty(digits)) {
-       //         return null;
-       //     }
-       //     int refult = 0;
-       //     foreach (var digit in digits.Split(' ')) {
-       //         int d;
-       //         if (int.TryParse(digit, out d)) {
-       //             refult += d;
-       //         } else {
-       //             return null;
-       //         }
-       //     }
-       //     return refult;
-       // }
+       public static int? GetSum(string digits) {
+           if (string.IsNullOrEmpty(digits)) {
+               return null;
+           }
+           int refult = 0;
+           foreach (var digit in digits.Split(' ')) {
+               int d;
+               if (int.TryParse(digit, out d)) {
+                   refult += d;
+               } else {
+                   return null;
+               }
+           }
+           return refult;
+       }
     }
 }
